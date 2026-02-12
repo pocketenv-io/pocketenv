dbce427324b35c6d0d40caf27be406c134f2ae39
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-02-13 01:24:17 +0300

    Add centered sign-in form with handle input
    Add AT Protocol handle input, Sign In button, and info links to
    selfhosted.social and Bluesky. Center layout and styling updated using
    Tailwind classes

--- apps/web/src/pages/signin/SignIn.tsx
+++ apps/web/src/pages/signin/SignIn.tsx
@@ -1,7 +1,37 @@
 function SignIn() {
   return (
     <>
-      <div className="flex min-h-screen bg-base-100"></div>
+      <div className="flex items-center justify-center min-h-screen bg-base-100">
+        <div className="flex flex-col items-center gap-6 w-[400px]">
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text text-[15px]">Handle</span>
+            </label>
+            <div className="input input-bordered w-full input-lg text-[15px] font-semibold bg-transparent">
+              <span className="label-text my-auto text-[16px] opacity-50 mr-[10px]">
+                @
+              </span>
+              <input placeholder="alice.bsky.social" className="grow " />
+            </div>
+          </div>
+
+          <button className="btn btn-primary btn-lg font-bold w-full">
+            Sign In
+          </button>
+          <p className="text-center text-white/70">
+            Don't have an atproto handle yet? You can create one at
+            <button className="text-[#8552ff]">selfhosted.social</button>,{" "}
+            <a
+              href="https://bsky.app/"
+              className="text-[#8552ff]"
+              target="_blank"
+            >
+              Bluesky
+            </a>{" "}
+            or any other AT Protocol service.
+          </p>
+        </div>
+      </div>
     </>
   );
 }

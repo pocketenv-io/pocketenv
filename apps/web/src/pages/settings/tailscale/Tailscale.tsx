d420322b800c68c026df32e32e24610f5be84bfc
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-03-08 16:20:08 +0300

    Add token input and Save button to Tailscale page

--- apps/web/src/pages/settings/tailscale/Tailscale.tsx
+++ apps/web/src/pages/settings/tailscale/Tailscale.tsx
@@ -20,12 +20,25 @@ function Tailscale() {
         <div className="w-[95%] m-auto">
           <div className="flex flex-row items-center">
             <h1 className="mb-2 text-xl flex-1">Tailscale</h1>
-            <button className="btn btn-primary font-semibold">Add Token</button>
           </div>
           <p className="opacity-60 mb-5">
             Connect your Sandbox to your Tailscale network for secure private
             access to services and devices.
           </p>
+          <div className="input input-bordered w-xl input-lg text-[15px] font-semibold bg-transparent">
+            <input
+              type="password"
+              className={`grow`}
+              placeholder="Enter your Tailscale token"
+              autoComplete="off"
+              data-1p-ignore
+              data-lpignore="true"
+              data-form-type="other"
+            />
+          </div>
+          <div>
+            <button className="btn btn-primary font-semibold mt-8">Save</button>
+          </div>
         </div>
       </>
     </Main>

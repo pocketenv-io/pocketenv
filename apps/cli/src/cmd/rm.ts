db093842a2545d772eba4ca4c343d7bc4f62fa7d
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-03-15 09:37:16 +0300

    Implement deleteSandbox API call and log

--- apps/cli/src/cmd/rm.ts
+++ apps/cli/src/cmd/rm.ts
@@ -1,3 +1,20 @@
-async function deleteSandbox(id: string) {}
+import consola from "consola";
+import { client } from "../client";
+import { env } from "../lib/env";
+import getAccessToken from "../lib/getAccessToken";
+
+async function deleteSandbox(id: string) {
+  const token = await getAccessToken();
+  client.post("/xrpc/io.pocketenv.sandbox.deleteSandbox", undefined, {
+    params: {
+      id,
+    },
+    headers: {
+      Authorization: `Bearer ${env.POCKETENV_TOKEN || token}`,
+    },
+  });
+
+  consola.success("Sandbox deleted successfully");
+}
 
 export default deleteSandbox;
